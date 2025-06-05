c1f5b865d236ac2e9496a6dfb0c96bb3faefb476
rajv69023 <rajv69023@gmail.com>
2025-06-05 15:43:49 +0530

    this is my second line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, batch39 ");
+        System.out.println("Hello, I'm raj and I'm currently studying in BCA ");
     }
 }
